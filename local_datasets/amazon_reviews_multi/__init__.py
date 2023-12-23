ded1a4d4bb60145b248facd97527b1b5a3dac832
= <=>
2023-12-23 17:52:54 +0100

    Adding 05_Vectorisation <3

--- local_datasets/amazon_reviews_multi/__init__.py
+++ local_datasets/amazon_reviews_multi/__init__.py
@@ -5,7 +5,7 @@ try:
     train = pd.read_csv('./local_datasets/amazon_reviews_multi/train.csv')
     test = pd.read_csv('./local_datasets/amazon_reviews_multi/test.csv')
     validation = pd.read_csv('./local_datasets/amazon_reviews_multi/validation.csv')
-    dataset = pd.concat([train, test, validation])
+    amazon_reviews_multi = pd.concat([train, test, validation])
 except:
     print('Please download the amazon_reviews_multi datasets here: https://www.kaggle.com/datasets/mexwell/amazon-reviews-multi')
     exit()
